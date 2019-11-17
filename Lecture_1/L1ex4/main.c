6010ba379349feb4428e63c635fd6e8d2645dc3c
Yuliya Sharkadiy <ysharkadiy@gmail.com>
2019-11-18 01:45:12 +0200

    Ex.4. is done.  Ex.4. Calculate perimeter and area of a circle.  Calculated perimeter 37.70 and area 113.10 (in meters)  Have a good day :)  Work was finished.

--- Lecture_1/L1ex4/main.c
+++ Lecture_1/L1ex4/main.c
@@ -4,22 +4,24 @@
  *  Created on: Nov 14, 2019
  *      Author: YS
  *
- *
- *
+ *      Ex4: Write a C program to compute the perimeter and area of
+ *      a circle with a radius of 6 meters.
  */
 #include <stdio.h>
-#include <stdint.h>
+#include <math.h>
 
 int main(void)
 {
-	setvbuf(stdout, NULL, _IONBF, 0);
-	setvbuf(stderr, NULL, _IONBF, 0);
-
 	// initialization of variable
+	float r = 6, perimeter, area;
 
-
+	printf(" Ex.4. Calculate perimeter and area of a circle. \n");
+	// calculate perimeter and area of rectangle
+	perimeter = 2 * M_PI * r;
+	area = M_PI * pow(r,2);
+	// print out results
+	printf(" Calculated perimeter %.2f and area %.2f (in meters) \n", perimeter, area);
 
 	printf(" Have a good day :) \n Work was finished.");
-
 	return 0;
 }
