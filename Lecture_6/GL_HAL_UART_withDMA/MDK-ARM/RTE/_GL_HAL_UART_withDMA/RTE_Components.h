ffc3301ddc3024e00a34b60364523a957c6255a8
Yuliya Sharkadiy <ysharkadiy@gmail.com>
2020-01-10 02:39:57 +0200

    Implemented UART driver with DMA using HAL API and Cube Mx code generator. Was used DMA in circular mode and normal with IT.

---
+++ Lecture_6/GL_HAL_UART_withDMA/MDK-ARM/RTE/_GL_HAL_UART_withDMA/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'GL_HAL_UART_withDMA' 
+ * Target:  'GL_HAL_UART_withDMA' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f3xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
