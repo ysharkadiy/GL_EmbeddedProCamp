8ff3c151664e6bd62d077da244c0a5009e0a5308
Yuliya Sharkadiy <ysharkadiy@gmail.com>
2020-01-20 22:01:10 +0200

    HomeWork for Memory Management was done in one project. Ex1.Create project to demonstrate static memory allocation. Ex2.Create project to demonstrate dynamic memory allocation.

---
+++ Lecture_10/GL_RTOS_Memory_Management/MDK-ARM/GL_RTOS_Memory_Management/croutine.d
@@ -0,0 +1,14 @@
+gl_rtos_memory_management\croutine.o: ../Middlewares/Third_Party/FreeRTOS/Source/croutine.c
+gl_rtos_memory_management\croutine.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h
+gl_rtos_memory_management\croutine.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+gl_rtos_memory_management\croutine.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+gl_rtos_memory_management\croutine.o: ../Inc/FreeRTOSConfig.h
+gl_rtos_memory_management\croutine.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
+gl_rtos_memory_management\croutine.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h
+gl_rtos_memory_management\croutine.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h
+gl_rtos_memory_management\croutine.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM4F/portmacro.h
+gl_rtos_memory_management\croutine.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+gl_rtos_memory_management\croutine.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h
+gl_rtos_memory_management\croutine.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h
+gl_rtos_memory_management\croutine.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/croutine.h
+gl_rtos_memory_management\croutine.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h
